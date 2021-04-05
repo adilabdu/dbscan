ff607edf68cbba633284488e41185eac3f7c3ce2
adilabdu <31595630+adilabdu@users.noreply.github.com>
2021-04-05 11:39:34 +0900

    DBSCAN class constructor, method and attributes

--- dbscan.h
+++ dbscan.h
@@ -4,6 +4,11 @@
 #include <vector>
 
 #define UNCLASSIFIED -1
+#define CORE_POINT 1
+#define BORDER_POINT 2
+#define NOISE -2
+#define SUCCESS 0
+#define FAILURE -3
 
 using namespace std;
 
@@ -14,5 +19,24 @@ typedef struct Point_ {
     int cluster_label;
 
 } Point;
+
+class DBSCAN {
+    public:
+    DBSCAN(unsigned int min_points, float epsilon, vector<Point> points) {
+        min_points = min_points;
+        epsilon = epsilon;
+        points = points;
+        size = points.size();
+    }
+
+    ~DBSCAN(){}
+
+    int run();
+
+    private:
+    int min_points;
+    int size;
+
+};
 
 #endif
