f2a45849fe6c0b3f431077abd6b86a8c1ea1f5b8
adilabdu <31595630+adilabdu@users.noreply.github.com>
2021-04-05 16:44:08 +0900

    DBSCAN class methods implementation

---
+++ dbscan.cpp
@@ -0,0 +1,99 @@
+#include "dbscan.h"
+
+int DBSCAN::run() {
+    
+    int cluster_id = 1;
+    
+    vector<Point>::iterator db_iter;
+    for(db_iter = points.begin(); db_iter != points.end(); db_iter++) {
+    
+        if(db_iter->cluster_id == UNCLASSIFIED) {
+    
+            if(expandCluster(*db_iter, cluster_id)) {
+                cluster_id += 1;
+            }
+    
+        }
+    }
+    
+    return 0;
+}
+
+bool DBSCAN::expandCluster(Point point, int cluster_id) {
+    
+    vector<int> cluster_seeds = regionQuery(point);
+
+    if(cluster_seeds.size() < min_points) {
+        point.cluster_id = NOISE;
+        return false;
+    } 
+    else {
+        int index = 0, index_core = 0;
+        
+        vector<int>::iterator seeds_iter;
+        for(seeds_iter = cluster_seeds.begin(); seeds_iter != cluster_seeds.end(); seeds_iter++) {
+
+            points.at(*seeds_iter).cluster_id = cluster_id;
+
+            if(points.at(*seeds_iter).point == point.point) {
+                index_core = index;
+            }
+            index++;
+        }
+
+        cluster_seeds.erase(cluster_seeds.begin() + index_core);
+
+        vector<int>::size_type i, n;
+        for(i = 0, n = cluster_seeds.size(); i < n; i++) {
+            vector<int> cluster_neighbors = regionQuery(points.at(cluster_seeds[i]));
+
+            if(cluster_neighbors.size() >= min_points) {
+                
+                vector<int>::iterator neighbors_iter;
+                for(neighbors_iter = cluster_neighbors.begin(); neighbors_iter != cluster_neighbors.end(); neighbors_iter++) {
+                    
+                    if(points.at(*neighbors_iter).cluster_id == UNCLASSIFIED ||
+                        points.at(*neighbors_iter).cluster_id == NOISE) {
+
+                            if(points.at(*neighbors_iter).cluster_id == UNCLASSIFIED) {
+                                cluster_seeds.push_back(*neighbors_iter);
+                                n = cluster_seeds.size();
+                            }
+                            points.at(*neighbors_iter).cluster_id = cluster_id;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
+
+vector<int> DBSCAN::regionQuery(Point point) {
+
+    int index = 0;
+    vector<int> cluster_index;
+
+    vector<Point>::iterator db_iter;
+    for(db_iter = points.begin(); db_iter != points.end(); db_iter++) {
+
+        if(distanceMeasure(point, *db_iter) <= epsilon) {
+            cluster_index.push_back(index);
+        } 
+    
+        index++;
+    }
+
+    return cluster_index;
+}
+
+inline float DBSCAN::distanceMeasure(Point poi, Point target) {
+    
+    float square_sums = 0;
+
+    for(int i = 0; i < dimension; i++) {
+        square_sums += pow(poi.point[i] - target.point[i], 2);
+    }
+
+    return sqrt(square_sums);
+}
